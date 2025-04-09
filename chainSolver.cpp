179f9c7b523cd3940739a41aa6e7c99f2b614ddd
Sirtage <zirocilt@gmail.com>
2025-04-09 09:42:47 +0300

    Solver now solves for I and U

--- chainSolver.cpp
+++ chainSolver.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
+#include <map>
+#include <list>
 #include <set>
 #define UDEF_V -1488
 
@@ -26,6 +28,8 @@ public:
     long double R = UDEF_V;
     std::vector<elem*> next;
     std::vector<elem*> prev;
+    //chain*
+    void* sub_chain = nullptr;
     elem() {
         this->next = std::vector<elem*>();
         this->prev = std::vector<elem*>();
@@ -82,6 +86,8 @@ public:
         this->prev = nw;
         return this;
     }
+
+    void* expand();
 };
 
 typedef class elem wire;
@@ -100,6 +106,7 @@ void mergeElements(elem* begin, elem* end, elem* R) {
 class chain
 {
 public:
+    elem* src;
     std::vector<wire*> wires;
     std::vector<elem*> apls;
     chain(std::vector<elem*> apls, std::vector<wire*> wires) {
@@ -147,7 +154,7 @@ public:
             if (w->next.size() == 1 && w->prev.size() == 1) {
                 //printf("%Lf : %Lf\n", w->next[0]->R, w->prev[0]->R);
                 if (w->next[0]->R != UDEF_V && w->prev[0]->R != UDEF_V) {
-                    if (w->next[0]->I != w->prev[0]->I) {
+                    if (w->next[0]->I != w->prev[0]->I && w->next[0]->I != UDEF_V && w->prev[0]->I != UDEF_V) {
                         v_error("Unequivalent I for straight connection.");
                         return false;
                     }
@@ -155,6 +162,12 @@ public:
                     R->R = w->next[0]->R + w->prev[0]->R;
                     R->I = w->prev[0]->I;
                     R->U = R->I*R->R;
+
+                    wire* w1 = (new wire())->from(w->next[0])->to(R);
+                    wire* w2 = (new wire())->from(R)->to(w->prev[0]);
+                    R->sub_chain = new chain({R, w->next[0], w->prev[0]}, {w, w1, w2});
+                    ((chain*)R->sub_chain)->src = R;
+
                     mergeElements(w->prev[0], w->next[0], R);
                     rmApls({w->prev[0], w->next[0]});
                     rmWire(w);
@@ -163,80 +176,163 @@ public:
                     return true;
                 }
             } else if (w->prev.size() != 1) {
-                bool flag = false, flag2 = false;
-                std::set<wire*> pts;
-                long double sumR = 0, sumI = 0;
+                std::map<elem*, std::vector<elem*>> mapper;
                 for (elem* p : w->prev) {
-                    if (p->R == UDEF_V) flag = true;
-                    pts.insert(p->prev[0]);
-                    sumR += 1/p->R;
-                    if (p->I == UDEF_V) flag2 = true;
-                    sumI += p->I;
+                    if (mapper.count(p->prev[0]) == 1) {
+                        mapper[p->prev[0]].push_back(p);
+                    } else {
+                        mapper[p->prev[0]] = std::vector<elem*>();
+                        mapper[p->prev[0]].push_back(p);
+                    }
                 }
-                if (flag || pts.size() != 1) continue;
-                // for (elem* v : w->prev) {
-                //     printf("!%Lf ", v);
-                // }
-                elem* R = new elem();
-                R->R = 1/sumR;
-                R->U = w->prev[0]->U;
-                R->I = flag2 ? UDEF_V : sumI;
-                rmApls(w->prev);
-                for (wire* pt : pts) {
-                    pt->excludeNexts(w->prev);
-                    w->prev.clear();
-                    w->from(R);
-                    pt->to(R);
-                    break;
+
+                for (std::pair<elem*, std::vector<elem*>> pr : mapper) {
+                    if (pr.second.size() > 1) {
+                        bool flag = false, flag2 = false;
+                        long double sumR = 0, sumI = 0;
+                        for (elem* p : pr.second) {
+                            if (p->R == UDEF_V) {
+                                flag = true;
+                                break;
+                            }
+                            sumR += 1/p->R;
+                            if (p->I == UDEF_V) flag2 = true;
+                            sumI += p->I;
+                        }
+                        if (flag) continue;
+                        // for (elem* v : w->prev) {
+                        //     printf("!%Lf ", v);
+                        // }
+                        elem* R = new elem();
+                        R->R = 1/sumR;
+                        R->U = w->prev[0]->U;
+                        R->I = flag2 ? UDEF_V : sumI;
+
+                        std::vector<elem*> subc_vec = std::vector<elem*>(pr.second);
+                        wire* w1 = (new wire())->from(R);
+                        wire* w2 = (new wire())->to(R);
+                        for (elem* ele : subc_vec) {
+                            w1->to(ele);
+                            w2->from(ele);
+                        }
+                        subc_vec.push_back(R);
+                        R->sub_chain = new chain(subc_vec, {w1, w2});
+                        ((chain*)R->sub_chain)->src = R;
+
+                        pr.first->excludeNexts(pr.second);
+                        w->excludePrevs(pr.second);
+                        pr.first->to(R);
+                        w->from(R);
+                        
+                        this->rmApls(pr.second);
+                        this->apls.push_back(R);
+                        printf("New parallel: %Lf\n", R->R);
+                        return true;
+                    }
                 }
-                this->apls.push_back(R);
-                printf("New parallel: %Lf\n", R->R);
-                return true;
             }
         }
         return false;
     }
+
+    void recursive_expand() {
+        for (elem* a : this->apls) {
+            chain* ptr = (chain*) a->expand();
+            if (ptr == this) continue;
+            if (ptr != nullptr) ptr->recursive_expand();
+        }
+    }
 };
 
+#define SKIP return nullptr;
+void* elem::expand() {
+    if (this->sub_chain != nullptr) {
+        chain* c = (chain*) this->sub_chain;
+        c->inc();
+        if (c->wires.size() == 3) {
+            if (this->I == UDEF_V) SKIP;
+            for (elem* e : c->apls) {
+                e->I = this->I;
+            }
+            c->inc();
+        }
+        else if (c->wires.size() == 2) {
+            if (this->U == UDEF_V) SKIP;
+            for (elem* e : c->apls) {
+                e->U = this->U;
+            }
+            c->inc();
+        }
+    }
+    return this->sub_chain;
+}
+#undef SKIP
 
-chain genChain() {
+
+// chain @ default_resistors_vec
+std::pair<chain, std::vector<elem*>> genChain() {
     //General definition
     elem* src = new elem();
-    src->R = 1;
+    src->R = 0;
+    src->U = 160;
     //Configuration
     //
     //
     //Definitions
     elem* R1 = new elem();
-    R1->R = 4;
+    R1->R = 5;
     elem* R2 = new elem();
-    R2->R = 3;
+    R2->R = 5;
     elem* R3 = new elem();
-    R3->R = 12;
+    R3->R = 5;
     elem* R4 = new elem();
-    R4->R = 6;
+    R4->R = 10;
+    elem* R5 = new elem();
+    R5->R = 10;
+    elem* R6 = new elem();
+    R6->R = 10;
+    elem* R7 = new elem();
+    R7->R = 10;
 
     //Wires and stuff
-    wire* w1 = (new wire())->from(src)->to(R1)->to(R3)->to(R4);
-    wire* w2 = (new wire())->from(R1)->from(R3)->to(R2);
-    wire* w3 = (new wire())->from(R4)->from(R2)->to(src);
+    wire* w1 = (new wire())->from(src)->to(R1);
+    wire* w2 = (new wire())->from(R1)->to(R4)->to(R2);
+    wire* w3 = (new wire())->from(R2)->to(R5)->to(R3);
+    wire* w4 = (new wire())->from(R3)->to(R6)->to(R7);
+    wire* w5 = (new wire())->from(R4)->from(R6)->from(R5)->from(R7)->to(src);
 
     //
     //
     //
+
+    std::vector<elem*> res_vec = {src, R1, R2, R3, R4, R5, R6, R7};
 
     //Genearal return
-    return chain({src, R1, R2, R3, R4}, {w1, w2, w3});
+    chain c = chain(std::vector<elem*>(res_vec), {w1, w2, w3, w4, w5});
+    c.src = src;
+    return std::pair<chain, std::vector<elem*>>(c, res_vec);
 }
 
 int main() {
-    chain c = genChain();
+    std::pair<chain, std::vector<elem*>> inp = genChain();
+    chain c = inp.first;
+    //c.inc();
     while (c.shrink()) {
         for (auto v : c.apls) {
             printf("%Lf ", v->R);
         }
         printf("\n");
-        c.shrink();
     }
+    c.src->R = c.apls[0]->R;
+    c.apls[0]->U = c.src->U;
+    c.apls[1]->I = c.src->I;
+
+    c.recursive_expand();
+    int i = 0;
+    for (elem* e : inp.second) {
+        printf("R%d->{I: %Lf, U: %Lf, R: %Lf};\n", i, e->I, e->U, e->R);
+        i++;
+    }
+    printf("\nFull chain built\n");
     return 0;
 }
